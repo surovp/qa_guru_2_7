d82c4ff773e0287cce6392ab95a5bb8d6e2a0947
Pavel Surov <surov_p@tanukitech.ru>
2022-10-03 20:16:09 +0500

    fixed browser.open_url

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,12 +8,11 @@ def browser_configs():
     browser.config.window_width = 1400
     browser.config.window_height = 1600
     browser.config.browser_name = 'chrome'
-    browser.config.base_url = "https://demoqa.com"
 
 
 @pytest.fixture()
 def open_and_close_form():
-    browser.open("/automation-practice-form")
+    browser.open_url("https://demoqa.com/automation-practice-form")
     yield
     browser.element("#closeLargeModal").double_click()
     browser.close()
